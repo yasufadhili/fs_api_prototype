fba6dc008e9fb1a29f7dfcd571d8ce1b6d8fdbc7
Yasu Fadhili <yasufadhili@gmail.com>
2023-10-21 08:50:44 +0300

    Initial commit

---
+++ fs_api_prototype/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fs_api_prototype project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fs_api_prototype.settings')
+
+application = get_wsgi_application()
