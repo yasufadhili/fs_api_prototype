df7d920843f9e6768f0217ea08b147933a6ba1b5
Yasu Fadhili <yasufadhili@gmail.com>
2024-10-04 06:49:10 +0100

    Updated settings

--- fs_api/urls.py
+++ fs_api/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "Football Social Admin"
+admin.site.site_title = "Football Social Admin"
+admin.site.index_title = "Football Social"
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
