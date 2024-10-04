9ab4c2939ca07135c0f7bb9a57af64dea2e31b3c
Yasu Fadhili <yasufadhili@gmail.com>
2024-10-04 07:53:54 +0100

    Added accounts urls

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'regular-profiles', views.RegularProfileViewSet)
+router.register(r'footballer-profiles', views.FootballerProfileViewSet)
+router.register(r'manager-profiles', views.ManagerProfileViewSet)
+router.register(r'organisation-profiles', views.OrganisationProfileViewSet)
+router.register(r'profile-statuses', views.ProfileStatusViewSet)
+router.register(r'user-relationships', views.UserRelationshipViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
