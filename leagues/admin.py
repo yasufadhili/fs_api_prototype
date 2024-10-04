a4253addaea5c8b23d5ac167ae7fe8af83592638
Yasu Fadhili <yasufadhili@gmail.com>
2024-10-04 07:14:07 +0100

    Added leagues admin

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import League
 
-# Register your models here.
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name', 'country', 'continent', 'year_established')
+    list_filter = ('continent', 'country')
+    search_fields = ('name', 'short_name', 'country__name', 'year_established')
+
+admin.site.register(League, LeagueAdmin)
