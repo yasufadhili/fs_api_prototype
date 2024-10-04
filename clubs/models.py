2502b00e9b570d2cbacc42352d5a735b6adbb883
Yasu Fadhili <yasufadhili@gmail.com>
2024-10-04 07:11:29 +0100

    Added club model

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+import shortuuid
+from django_countries.fields import CountryField
+from autoslug import AutoSlugField
+
+from leagues.models import League
+
+
+class Club(models.Model):
+    id = models.CharField(_(u'id'),
+                          primary_key=True,
+                          max_length=255,
+                          default=shortuuid.ShortUUID().random(4),
+                          help_text=u'Club ID',
+                          db_index=True)
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name', unique=True)
+    full_name = models.CharField(max_length=155)
+    short_name = models.CharField(max_length=55)
+    logo = models.URLField(blank=True, null=True)
+    year_established = models.SmallIntegerField(blank=True, null=True)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, **kwargs):
+        self.slug = slugify(self.name)
+        super().save()
