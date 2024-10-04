f404dc794c0cb17678e5e0dd6629518cb272439e
Yasu Fadhili <yasufadhili@gmail.com>
2024-10-04 07:12:40 +0100

    Added clubs admin

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Club
+from leagues.models import League
 
-# Register your models here.
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name', 'full_name', 'year_established', 'league')
+    list_filter = ('league',)
+    search_fields = ('name', 'short_name', 'full_name', 'year_established')
+
+
+admin.site.register(Club, ClubAdmin)
